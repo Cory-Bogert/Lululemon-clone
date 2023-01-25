9a36ac2a00c30df3d68a5d23854496503cc25c55
cory-bogert <bogert.cory@gmail.com>
2023-01-25 09:39:10 -0600

    pushing to render

---
+++ app/seeds/item_seed.py
@@ -0,0 +1,31 @@
+from app.models import db, Item, environment, SCHEMA
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_items():
+    item1 = Item(
+        name='At-ease shorts', category='Shorts', color='blue', price=78.99, size=2, stocked=True, previewImg='https://images.lululemon.com/is/image/lululemon/LM7AI9S_051860_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72')
+    item2 = Item(
+        name='At-ease shorts2', category='Shorts', color='yellow', price=78.99, size=4, stocked=True, previewImg='https://images.lululemon.com/is/image/lululemon/LM7AI9S_051860_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72')
+    item3 = Item(
+        name='At-ease shorts3', category='Shorts', color='red', price=78.99, size=6, stocked=True, previewImg='https://images.lululemon.com/is/image/lululemon/LM7AI9S_051860_1?wid=1080&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72')
+
+    db.session.add(item1)
+    db.session.add(item2)
+    db.session.add(item3)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_items():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM items")
+
+    db.session.commit()
---
+++ app/seeds/purchased_item_seed.py
@@ -0,0 +1,31 @@
+from app.models import db, PurchasedItem, environment, SCHEMA
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_purchased_item():
+    purchased_item1 = PurchasedItem(
+        userId=1, itemId=1)
+    purchased_item2 = PurchasedItem(
+        userId=2, itemId=2)
+    purchased_item3 = PurchasedItem(
+        userId=3, itemId=3)
+
+    db.session.add(purchased_item1)
+    db.session.add(purchased_item2)
+    db.session.add(purchased_item3)
+    db.session.commit()
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_purchased_item():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM purchased_items")
+
+    db.session.commit()
