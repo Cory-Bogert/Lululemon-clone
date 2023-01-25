9fdde86c2934bce2ba7846713aed9f4990f23c39
cory-bogert <bogert.cory@gmail.com>
2023-01-25 15:51:28 -0600

    checking validator again

--- react-app/src/store/item.js
+++ react-app/src/store/item.js
@@ -1,2 +1,57 @@
 //action types
 const READ_ITEMS = 'items/READ_ITEMS'
+const READ_SINGLE_ITEM = 'items/READ_SINGLE_ITEM'
+
+
+const getAll = ({Items}) => ({
+    type: READ_ITEMS,
+    Items
+})
+
+const getOne = (item) => ({
+    type: READ_SINGLE_ITEM,
+    item
+})
+
+//thunks
+export const fetchAllItems = () => async dispatch => {
+    const response = await fetch(`/api/items`);
+    if(response.ok){
+        const itemList = await response.json()
+        dispatch(getAll(itemList))
+    }
+    if(response.status>=400) throw response
+}
+
+export const fetchOneItem = (itemId) => async dispatch => {
+    const response = await fetch(`/api/items/${itemId}`)
+    if(response.ok){
+        const singleItem = await response.json()
+        dispatch(getOne(singleItem))
+    }
+    if(response.status>=400) throw response
+}
+
+//reducer
+const initalState = {}
+const itemsReducer = (state = initalState, action) => {
+    let newState;
+    switch(action.type){
+        case READ_ITEMS:
+            newState={...state}
+            action.Items.forEach(item => {
+                newState[item.id] = item
+            })
+            return newState
+
+        case READ_SINGLE_ITEM:
+            newState = {...state}
+            newState[action.item.id] = action.item
+            return newState
+
+        default:
+            return state
+    }
+}
+
+export default itemsReducer
