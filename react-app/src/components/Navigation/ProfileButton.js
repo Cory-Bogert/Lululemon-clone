d1194739174d8047e70469f2a8ca1e26eb15b677
cory-bogert <bogert.cory@gmail.com>
2023-02-03 16:51:48 -0600

    lots of styling, going to test carousel

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,54 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import LoginFormModal from '../LoginFormModal';
-import DemoUser from '../Demo';
-import LogoutButton from '../auth/LogoutButton';
-import CartButtonModal from '../Carts/CartButtonModal';
-import './index.css'
-import SignupFormModal from '../SignupFormModal';
+
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import { useHistory, NavLink } from 'react-router-dom'
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
+import LoginForm from "../auth/LoginForm";
+import SignUpForm from "../auth/SignUpForm";
+import DemoUser from "../Demo";
+import { Modal } from "../../context/Modal";
+
+function ProfileButton({ user, setLogin, setShowModal }) {
+  const dispatch = useDispatch();
+  const history = useHistory()
+  const [showMenu, setShowMenu] = useState(false);
+  const [showLoginModal, setShowLoginModal] = useState(false)
+  const [showSignUpModal, setShowSignUpModal] = useState(false)
+  const sessionUser = useSelector(state => state.session.user)
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+    setShowLoginModal(false)
+    setShowSignUpModal(false)
+    history.push('/')
+  };
 
 
-function Navigation({ isLoaded }){
-    const sessionUser = useSelector(state => state.session.user)
-    // const sessionLinks = ()
-
+  if(sessionUser){
     return (
-        <header>
-        <nav className='navbar-container'>
-            {/* <div className='logo-container'>
-
-
-            </div> */}
-
-            <div className='welcome-container'>{sessionUser ? (`Welcome, ${sessionUser.username}!`): ('Welcome to LuluMelon')}</div>
-            <div className='logo-name'>
-              <NavLink className='logo-name' exact to='/'>
-                LuluMelon
-                {/* <img className='logo-img' src={<i class="fa-solid fa-watermelon-slice"></i>}></img> */}
-              </NavLink></div>
-            <div className='navbar-right'>
-            <div className='btn-container'>
-              {sessionUser ? null : <LoginFormModal/>}
-              {sessionUser ? null : <SignupFormModal/>}
-              <DemoUser />
-              <LogoutButton />
-              <CartButtonModal />
-            </div>
-
-            <div className='user-container'>
-              <div className='profile-dropdown'>
-                {isLoaded
-                //  && sessionLinks
-                 }
-              </div>
-            </div>
+      <>
+        <button className='user-profile-btn' onClick={openMenu}>
+          <div className='user-proflie-icons'>
+            <i className='navbar-user-circle fas fa-user-circle fa-2x'></i>
           </div>
+        </button>
 
-
-        </nav>
-      </header>
-
+        {showMenu && (
+          <ul className='user-dropdown'>
+            <ul className='user-info'>Welcome {user.firstName}</ul>
+            <ul className='user-info'>{user.email}</ul>
+            <ul>
+              <button className='logout-btn' onClick={logout}>Logout</button>
+            </ul>
+          </ul>
+        )}
+      </>
     )
-    }
-  export default Navigation
+  } else {
+    return (
+      <>
+        <button className='user-profile-btn' onClick={openMenu}>
+          <div className='user-profile-icons'>
+            <i className='navbar-user-circle fas fa-user-circle fa-2x'></i>
+          </div>
+        </button>
+
+        {showMenu && (
+          <ul className='user-dropdown'>
+            <li className='modal-dropdown' onClick={() => setShowSignUpModal(true)}><SignupFormModal /></li>
+            <li className='modal-dropdown' onClick={() => setShowLoginModal(true)}><LoginFormModal /></li>
+          </ul>
+        )}
+
+        {showLoginModal && (
+          <Modal onClose={() => setShowLoginModal(false)}>
+          <LoginForm />
+          {/* <DemoUser /> */}
+          </Modal>
+        )}
+
+        {showSignUpModal && (
+          <Modal onClose={() => setShowSignUpModal(false)}>
+            <SignupFormModal />
+          </Modal>
+        )}
+
+      </>
+    )
+  }
+
+}
+
+export default ProfileButton
